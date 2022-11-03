8af8ce9ccb573d25a26633f4dc74d8fb38d3ba99
jieke6668 <3252969500@qq.com>
2022-11-03 20:25:27 +0800

    完成地图和examples图标

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,105 @@
 // index.js
+import * as echarts from '../../ec-canvas/echarts';
+
 // 获取应用实例
 const app = getApp()
 const { getList } = require('../../plugins/apis')
+function initChart(canvas, width, height) {
+    const chart = echarts.init(canvas, null, {
+        width: width,
+        height: height
+    });
+    canvas.setChart(chart);
+
+    var option = {
+        title: {
+            text: 'Stacked Line'
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        toolbox: {
+            feature: {
+                saveAsImage: {}
+            }
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+                name: 'Email',
+                type: 'line',
+                stack: 'Total',
+                data: [120, 132, 101, 134, 90, 230, 210]
+            },
+            {
+                name: 'Union Ads',
+                type: 'line',
+                stack: 'Total',
+                data: [220, 182, 191, 234, 290, 330, 310]
+            },
+            {
+                name: 'Video Ads',
+                type: 'line',
+                stack: 'Total',
+                data: [150, 232, 201, 154, 190, 330, 410]
+            },
+            {
+                name: 'Direct',
+                type: 'line',
+                stack: 'Total',
+                data: [320, 332, 301, 334, 390, 330, 320]
+            },
+            {
+                name: 'Search Engine',
+                type: 'line',
+                stack: 'Total',
+                data: [820, 932, 901, 934, 1290, 1330, 1320]
+            }
+        ]
+    };
+    chart.setOption(option);
+    return chart;
+}
+
 Page({
     data: {
-        content: []
+        latitude: '',
+        longitude: '',
+        content: [],
+        ec: {
+            onInit: initChart
+        }
     },
+
     onLoad() {
+        wx.getLocation({
+            type: 'gcj02',
+            success: res => {
+                //赋值经纬度
+                console.log(res.latitude);
+                console.log(res.longitude);
+                this.setData({
+                    latitude: res.latitude,
+                    longitude: res.longitude,
+                })
+            }
+        })
         getList({}).then(res => {
             res.data.data.content.forEach(ele => {
                 let date = new Date(ele.dateOfRegistration);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
@@ -20,5 +113,7 @@ Page({
             })
             // console.log(res.data.data.content);
         })
-    }
+    },
+
+
 })
--- plugins/apis.js
+++ plugins/apis.js
@@ -5,13 +5,13 @@ const axios = require('./axios')
 
 
 const baseUrl = "https://api.tf2sc.cn/api/tfcar/car"
-const baseUrls="https://api.tf2sc.cn"
+const baseUrls = "https://api.tf2sc.cn"
 
 // 获取列表
 export function getList(obj) {
 	return axios({
 		url: baseUrl + "/list?",
-		data:{
+		data: {
 			...obj
 		}
 	})
@@ -27,8 +27,8 @@ export function getParticulars(obj) {
 //获取地区
 export function area(obj) {
 	return axios({
-		url:baseUrls+"/api/tfcar/city/treeList",
-		data:{
+		url: baseUrls + "/api/tfcar/city/treeList",
+		data: {
 			...obj
 		}
 	})
@@ -36,35 +36,35 @@ export function area(obj) {
 //搜索接口
 export function seach(obj) {
 	return axios({
-		url:baseUrls+"/api/tfcar/car/list",
-		data:{
+		url: baseUrls + "/api/tfcar/car/list",
+		data: {
 			...obj
 		}
 	})
 }
 //车辆品牌接口
-export function brand(obj){
+export function brand(obj) {
 	return axios({
-		url:baseUrl+"/brand300",
-		data:{
+		url: baseUrl + "/brand300",
+		data: {
 			...obj
 		}
 	})
-} 
+}
 //车辆品牌子集接口
 export function brandclicnd(obj) {
 	return axios({
-		  url:baseUrl + "/series300?",
-		  data:{
-			  ...obj
-		  }
+		url: baseUrl + "/series300?",
+		data: {
+			...obj
+		}
 	})
 }
 //价格请求接口
 export function price(obj) {
 	return axios({
-		url:baseUrl+"/price?",
-		data:{
+		url: baseUrl + "/price?",
+		data: {
 			...obj
 		}
 	})
@@ -72,8 +72,8 @@ export function price(obj) {
 //车型请求接口
 export function vehicle(obj) {
 	return axios({
-		url:baseUrl+"/model",
-		data:{
+		url: baseUrl + "/model",
+		data: {
 			...obj
 		}
 	})
